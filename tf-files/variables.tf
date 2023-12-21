45666f2a779dafb3c2c0110ba6eeeacea4df4f4b
Sahin Erken <140431101+erkensahin@users.noreply.github.com>
2023-12-21 23:43:40 +0300

    last version

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "erkensahin"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "sahindevops.net"
+}
